fed992436456114635608ed3eca92962077cc418
SirZompiGit <roby96dark@gmail.com>
2026-07-21 18:04:47 +0200

    Delete audio-patch.ts

--- audio-patch.ts
+++
@@ -1,9 +0,0 @@
-let isMuted = false;
-if (typeof window !== 'undefined') {
-  isMuted = localStorage.getItem('fantasia_muted') === 'true';
-  window.addEventListener('storage', (e) => {
-    if (e.key === 'fantasia_muted') {
-      isMuted = e.newValue === 'true';
-    }
-  });
-}
